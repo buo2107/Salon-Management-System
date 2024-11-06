85f028b0660c07fdbb57a877441f7726ffb4c220
buo2107 <vampire91145@gmail.com>
2024-11-06 20:37:47 +0800

    RESTART

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,5 @@
 function App() {
-  return (
-    // <BrowserRouter>
-    //   <HeaderNav />
-
-    //   <Box as="section" h="100%" px={24} py={8} overflow={"hidden"}>
-    //     <Routes>
-    //       <Route index path="home" element={<HomePage />} />
-    //       <Route path="customers" element={<Customers />} />
-    //       <Route path="product" element={<Catalog />} />
-    //       <Route path="sales" element={<SalesDetails />} />
-    //       <Route path="calander" element={<Calander />} />
-    //       <Route path="financial" element={<FinancialState />} />
-    //     </Routes>
-    //   </Box>
-    // </BrowserRouter>
-  );
+  return <div></div>;
 }
 
 export default App;
